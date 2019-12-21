9d5c5504fa83546360979b11af55006cff03e03a
Ailson Freire <ailson_freire@outlook.com>
2019-12-20 22:21:11 -0300

    Getting stations from the API

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { getStations } from "@services/stationsAPI";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import { useAsyncEffect } from "use-async-effect";
 
 // import { Container } from './styles';
 const Home = () => {
+  useAsyncEffect(async () => {
+    const response = await getStations();
+    console.log(response)
+  });
+
+
   return (
     <View />
   );
